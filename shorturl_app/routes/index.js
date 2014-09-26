980d6c05b92947650210cb5122d38b66e89d3603
tankony <itoama@gmail.com>
2014-09-27 04:07:32 +0900

    fix short_url

--- shorturl_app/routes/index.js
+++ shorturl_app/routes/index.js
@@ -8,7 +8,7 @@ var async = require('async'),
 
 //initial view
 exports.index = function(req, res){
-  res.render('index', { new_url: '変換したいURLを入力して変換ボタンを押してください' });
+  res.render('index', { new_url: 'ここに変換後のURLが表示されます。。。' });
 };
 
 
@@ -18,28 +18,55 @@ exports.register = function(req, res){
   var url = req.body.url;
 
   async.waterfall([
+    //URLの検証
+    function(_callback) {
+      if( /^https?:\/\/.+\..+/.test(url)) {
+        _callback(null);
+      }
+      //入力内容が不正な場合
+      else {
+        return _callback({message: '入力内容が不正です。。。'});
+      }
+    },
+    //URLが既に登録済みかどうかの判定
     function(_callback) {
       util.isRegistUrl(url, _callback);
     }
   ], function(err, result) {
-    //登録済の場合
-    if(result) {
-      res.render('index', { new_url: 'http://kontany.net:9999/'+result });
+    //入力内容不正な場合
+    if(err) {
+      res.render('index', { new_url: err.message });
     }
-
-    //未登録の場合
     else {
-      async.waterfall([
-        function(_callback) {
-          util.insertUrl(url, _callback);
-        },
-        function(id, _callback) {
-          var enc_id = util.base64_enc(String(id));
-          _callback(null, enc_id);
-        }
-      ], function (err, enc_id) {
-        res.render('index', { new_url: 'http://kontany.net:9999/'+ enc_id });
-      });
+      //登録済の場合
+      if(result) {
+        res.render('index', { new_url: 'http://kontany.net:9999/'+result });
+      }
+  
+      //未登録の場合
+      else {
+        async.waterfall([
+          function(_callback) {
+            util.insertUrl(url, _callback);
+          },
+          function(id, _callback) {
+            var enc_id = util.base64_enc(String(id));
+            _callback(null, enc_id);
+          }
+        ], function (err, enc_id) {
+          
+          var res_word;
+          console.log(err);
+  
+          if(err) {
+            res_word = err;
+          }
+          else {
+            res_word = 'http://kontany.net:9999/'+ enc_id;
+          }
+          res.render('index', { new_url: res_word });
+        });
+      }
     }
   });
 };
